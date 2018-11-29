373781f61a2207f335ea8dd78e371ed24d304464
cyrg <caiqiang@example.com>
2018-11-29 23:40:24 +0800

    wanshang

--- blogs/myblog/views.py
+++ blogs/myblog/views.py
@@ -8,12 +8,14 @@ from django.shortcuts import render, redirect
 # Create your views here.
 from django.urls import reverse
 
-from adminmanage.models import Blog, Comments, User
+from adminmanage.models import Blog, Comments, User, Visitnum
 
 
 def blog(request,num='1'):
 
     uname = request.session.get('name')
+    frontblogs = Blog.bmymanager.filter().order_by('vistnums').reverse()
+    frontblog = frontblogs[0:3]
     blogs = Blog.bmymanager.all()
     paginator = Paginator(blogs, 3)
     # print(paginator.count,paginator.page_range)
@@ -33,36 +35,43 @@ def blog(request,num='1'):
         customRange = paginator.page_range
     return render(request,'myblog/index.html',context={'blogs':page.object_list,
         'pagerange':customRange, # 页码范围
-        'page':page
+        'page':page,
+        'frontblogs':frontblog,
         })
 
 
 def showblog(request,id):
-    blog = Blog.bmymanager.get(pk=int(id))
-    lastid = Blog.bmymanager.last().id
-    path =request.path
-    comments = Comments.commanager.filter(blog_id=int(id))
-    bodypath = blog.body.split('blogs')[1]
-    data= {
-        'blog':blog,
-        'comments': comments,
-        'content':bodypath,
-        'path':path,
-    }
+        uname = request.session.get('uname')
+        blog = Blog.bmymanager.get(pk=int(id))
+        visitnums = blog.vistnums
+        blog.vistnums = visitnums + 1
+        blog.save()
+        path =request.path
+        comments = Comments.commanager.filter(blog_id=int(id))
+        bodypath = blog.body.split('blogs')[1]
+        data= {
+            'blog':blog,
+            'comments': comments,
+            'content':bodypath,
+            'path':path,
+        }
 
-    return render(request,'myblog/article.html',context=data)
+        return render(request,'myblog/article.html',context=data)
 
 
 def submitcomment(request):
-    uid = User.umanager.get(uname=request.session.get('uname')).id
-    comment = Comments()
-    bid = request.GET.get('bid')
-    blog = Blog.bmymanager.get(pk=int(bid))
-    commentbody = request.GET.get('comments')
-    comment.blog_id = int(bid)
-    comment.user_id = uid
-    comment.body = comment.body
-    comment.save()
-    commentnums = blog.commentnums
-    blog.commentnums = commentnums + 1
-    return JsonResponse({'code':'1'})
+    if request.session.get('uname'):
+        uid = User.umanager.get(uname=request.session.get('uname')).id
+        comment = Comments()
+        bid = request.GET.get('bid')
+        blog = Blog.bmymanager.get(pk=int(bid))
+        commentbody = request.GET.get('comments')
+        comment.blog_id = int(bid)
+        comment.user_id = uid
+        comment.body = comment.body
+        comment.save()
+        commentnums = blog.commentnums
+        blog.commentnums = commentnums + 1
+        return JsonResponse({'code':'1'})
+    else:
+        return redirect(reverse('myManage:login'))
